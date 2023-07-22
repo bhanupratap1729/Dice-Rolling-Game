cd2a1138a93569d8df1db3414f059fbffae802ce
BHANU PRATAP <bhanu.spratap01@gmail.com>
2023-07-22 22:55:22 +0530

    Add files via upload

---
+++ Dice-game/script.js
@@ -0,0 +1,39 @@
+// accessing the rolls numbers
+
+
+const  oneE1 = document.querySelector("one");
+const  twoE1 = document.querySelector("two");
+const  threeE1 = document.querySelector("three");
+const  fourE1 = document.querySelector("four");
+const  fiveE1 = document.querySelector("five");
+const  sixE1 = document.querySelector("six");
+
+//accessing the dice
+const diceE1 = document.querySelector(".dice");
+
+
+// // click event to all the rolls numbers
+oneE1.addEventListener("click", ()=>{
+    diceE1.style.transform=`rotateX(${180}deg)
+    rotateY(${100}deg) rotateZ(${180}deg)`;
+})
+twoE1E1.addEventListener("click", ()=>{
+    diceE1.style.transform=`rotateX(${180}deg)
+    rotateY(${100}deg) rotateZ(${180}deg)`;
+})
+threeE1.addEventListener("click", ()=>{
+    diceE1.style.transform=`rotateX(${180}deg)
+    rotateY(${100}deg) rotateZ(${180}deg)`;
+})
+fourE1.addEventListener("click", ()=>{
+    diceE1.style.transform=`rotateX(${180}deg)
+    rotateY(${100}deg) rotateZ(${180}deg)`;
+})
+fiveE1.addEventListener("click", ()=>{
+    diceE1.style.transform=`rotateX(${180}deg)
+    rotateY(${100}deg) rotateZ(${180}deg)`;
+})
+sixE1.addEventListener("click", ()=>{
+    diceE1.style.transform=`rotateX(${180}deg)
+    rotateY(${100}deg) rotateZ(${180}deg)`;
+})
